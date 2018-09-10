0841e75c13f0ce9830ba1f2e4b12c6a32479baeb
Nutchanun <60050190@kmitl.ac.th>
2018-09-10 19:14:32 +0700

    fix +

---
+++ Adder.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Adder {
+    public static void main(String[] args) {
+        System.out.println("Hello cs!");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z ;
+        z = x+y;
+        System.out.println(z);
+
+
+
+    }
+}
+
+
+
+    
